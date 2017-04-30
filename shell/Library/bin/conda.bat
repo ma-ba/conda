41cf94360b929249028084947ceeb8fa0674276d
Marcel Bargull <marcel.bargull@udo.edu>
2017-04-30 18:10:11 +0200

    consolidate code in conda.bat

--- shell/Library/bin/conda.bat
+++ shell/Library/bin/conda.bat
@@ -16,45 +16,55 @@
 
 
 :DO_ACTIVATE
-@IF "%CONDA_PROMPT_MODIFIER%" == "" GOTO skip_prompt_set_activate
-    @CALL SET "PROMPT=%%PROMPT:%CONDA_PROMPT_MODIFIER%=%_empty_not_set_%%%"
-:skip_prompt_set_activate
-@SET "_TEMP_SCRIPT_PATH="
-@FOR /F "delims=" %%i IN ('CMD /C @CALL "%_CONDA_EXE%" shell.activate cmd.exe %2 %3 ^|^| ECHO _ERR_LVL_%%^^ERRORLEVEL%%') DO @SET "_TEMP_SCRIPT_PATH=%%i"
-@REM if call failed with error code x, _TEMP_SCRIPT_PATH is set to string _ERR_LVL_x
-@IF DEFINED _TEMP_SCRIPT_PATH GOTO check_error_activate
-    @REM TODO: handle zero length output
-    @GOTO End
-:check_error_activate
-@IF NOT "%_TEMP_SCRIPT_PATH:~0,9%" == "_ERR_LVL_" GOTO call_temp_script_activate
-    @SET "_ERR_LVL=%_TEMP_SCRIPT_PATH:~9%"
-    @REM TODO: handle failed call which returned error code %_ERR_LVL%
-    @GOTO cleanup_activate
-:call_temp_script_activate
-@CALL "%_TEMP_SCRIPT_PATH%"
-@DEL /F /Q "%_TEMP_SCRIPT_PATH%"
-@SET PROMPT="%CONDA_PROMPT_MODIFIER%%PROMPT%"
-:cleanup_activate
-@SET _TEMP_SCRIPT_PATH=
+@CALL :CALL_COMMAND_WITH_PROMPT_UPDATE shell.activate %2 %3
+@IF NOT ERRORLEVEL 1 GOTO :End
+@REM special error handling for activate
 @GOTO :End
 
 :DO_DEACTIVATE
-@IF "%CONDA_PROMPT_MODIFIER%" == "" GOTO skip_prompt_set_deactivate
-    @CALL "SET PROMPT=%%PROMPT:%CONDA_PROMPT_MODIFIER%=%_empty_not_set_%%%"
-:skip_prompt_set_deactivate
-@FOR /F "delims=" %%i IN ('@CALL "%_CONDA_EXE%" shell.deactivate cmd.exe %2') DO @SET "_TEMP_SCRIPT_PATH=%%i"
-@CALL "%_TEMP_SCRIPT_PATH%"
-@DEL /F /Q "%_TEMP_SCRIPT_PATH%"
-@SET _TEMP_SCRIPT_PATH=
-@SET "PROMPT=%CONDA_PROMPT_MODIFIER%%PROMPT%"
+@CALL :CALL_COMMAND_WITH_PROMPT_UPDATE shell.deactivate %2
+@IF NOT ERRORLEVEL 1 GOTO :End
+@REM special error handling for deactivate
 @GOTO :End
 
 :DO_REACTIVATE
-@FOR /F "delims=" %%i IN ('@CALL "%_CONDA_EXE%" shell.reactivate cmd.exe %2') DO @SET "_TEMP_SCRIPT_PATH=%%i"
+@CALL :CALL_COMMAND shell.reactivate %2
+@IF NOT ERRORLEVEL 1 GOTO :End
+@REM special error handling for reactivate
+@GOTO :End
+
+
+:CALL_COMMAND_WITH_PROMPT_UPDATE
+@IF "%CONDA_PROMPT_MODIFIER%" == "" GOTO skip_prompt
+    @CALL SET "PROMPT=%%PROMPT:%CONDA_PROMPT_MODIFIER%=%_empty_not_set_%%%"
+:skip_prompt
+@CALL :CALL_COMMAND %*
+@IF NOT ERRORLEVEL 1 GOTO :update_prompt
+    @SET "CONDA_PROMPT_MODIFIER="
+    @EXIT /B 1
+:update_prompt
+@SET PROMPT="%CONDA_PROMPT_MODIFIER%%PROMPT%"
+@EXIT /B 0
+
+:CALL_COMMAND
+@SET "_TEMP_SCRIPT_PATH="
+@FOR /F "delims=" %%i IN ('CMD /C @CALL "%_CONDA_EXE%" %1 cmd.exe %2 %3 ^|^| ECHO _ERR_LVL_%%^^ERRORLEVEL%%') DO @SET "_TEMP_SCRIPT_PATH=%%i"
+@REM if call failed with error code x, _TEMP_SCRIPT_PATH is set to string _ERR_LVL_x
+@IF DEFINED _TEMP_SCRIPT_PATH GOTO check_error
+    @REM TODO: handle zero length output
+    @EXIT /B 1
+:check_error
+@IF NOT "%_TEMP_SCRIPT_PATH:~0,9%" == "_ERR_LVL_" GOTO call_temp_script
+    @SET "_ERR_LVL=%_TEMP_SCRIPT_PATH:~9%"
+    @REM TODO: handle failed call which returned error code %_ERR_LVL%
+    @SET _TEMP_SCRIPT_PATH=
+    @EXIT /B %_ERR_LVL%
+:call_temp_script
 @CALL "%_TEMP_SCRIPT_PATH%"
 @DEL /F /Q "%_TEMP_SCRIPT_PATH%"
 @SET _TEMP_SCRIPT_PATH=
-@GOTO :End
+@EXIT /B 0
+
 
 :End
 @SET _CONDA_EXE=
